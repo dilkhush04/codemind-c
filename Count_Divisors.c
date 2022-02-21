61f58fc30e6d1d8a6c05678224f48fb6cd87a738
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-02-21 18:50:45 +0530

    #include<stdio.h> int main() { int n,m,i,s=0,j=1,k; scanf("%d%d%d",&n,&m,&k); i=k; for(n%i==0;n<=m;n++)
    if(n%i==0)    
    s=s+j;j++;
       
    printf("%d",s);   
    return 0;}

---
+++ Count_Divisors.c
@@ -0,0 +1,13 @@
+#include<stdio.h>  
+int main()    
+{    
+int n,m,i,s=0,j=1,k;    
+scanf("%d%d%d",&n,&m,&k); 
+i=k;
+for(n%i==0;n<=m;n++)    
+   
+if(n%i==0)    
+s=s+j;j++;
+   
+printf("%d",s);   
+return 0;}
