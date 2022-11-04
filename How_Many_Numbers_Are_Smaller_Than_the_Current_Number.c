d751fe129d08fe43f606e672d755f73ea3bdfbca
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-11-04 20:08:31 +0530

    #include<stdio.h> int main() {     int i,j,n,a[100],c;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(a[i]>a[j])             {                 c++;             }         }         printf("%d ",c);     } }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,a[100],c;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                c++;
+            }
+        }
+        printf("%d ",c);
+    }
+}
