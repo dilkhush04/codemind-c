0fcaf83de7365e2d7a8ca61cff4e338ed562af01
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-02 21:57:46 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d%d",&n,&m);   int   x=n<m?n:m;     while(1>0)     {         if(x>0)         {         if(n%x==0 && m%x==0)         {             printf("%d",x);             break;         }         x--;         }     } }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 int main()
 {
-int a,b,h;
-scanf("%d%d",&a,&b);
-for(h=a<b?a:b;h>=1;h--)
-if(a%h==0&&b%h==0)
-break;
-printf("%d",h);
-return 0;
+    int n,m;
+    scanf("%d%d",&n,&m);
+  int   x=n<m?n:m;
+    while(1>0)
+    {
+        if(x>0)
+        {
+        if(n%x==0 && m%x==0)
+        {
+            printf("%d",x);
+            break;
+        }
+        x--;
+        }
+    }
 }
