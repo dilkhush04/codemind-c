840b3b29da7e5af81c891225c476cde1c794e75c
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-03 18:08:20 +0530

    #include <stdio.h>
    int main()
    
    {
    
        int i, num, temp = 0;
    
        scanf("%d", &num);
    
        for (i = 2; i <= num / 2; i++)
        {
            if (num % i == 0)
            {
                temp=1;
                break;
            }
        } 
        if (temp==0)
        {
            printf("prime");
        }
        else
        {
            printf("not a prime");
        }
        return 0;
    }

--- prime_number.c
+++ prime_number.c
@@ -8,43 +8,21 @@ int main()
 
     scanf("%d", &num);
 
-    // iterate up to n/2.
-
     for (i = 2; i <= num / 2; i++)
-
     {
-
-        // check if num is divisible by any number.
-
         if (num % i == 0)
-
         {
-
             temp=1;
-
             break;
-
         }
-
     } 
-
-
     if (temp==0)
-
     {
-
         printf("prime");
-
     }
-
     else
-
     {
-
         printf("not a prime");
-
     }
-
     return 0;
-
 }
