82f3dab6cf7471d887806185c13fedf4c7c1a2ef
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-04 10:10:22 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int i,j,a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         if(a[i]%2!=0)         {             printf("False");             return 0;         }
        }
        printf("True");
        return 0;
    }

---
+++ Even_Array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i,j,a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]%2!=0)
+        {
+            printf("False");
+            return 0;
+        }
+        
+    }
+    printf("True");
+    return 0;
+}
