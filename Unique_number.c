b164bb79a604849ef97a992c4820bf3cc4f9ac82
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-08 18:01:59 +0530

    #include<stdio.h> int main() {     int n,a[1000],k=0,i,j;     scanf("%d",&n);     while(n!=0)     {         a[k++]=n%10;         n/=10;     }     for(i=0;i<k;i++)     {         for(j=i+1;j<k;j++)         {             if(a[i]==a[j])             {                 printf("Not Unique Number");                 return 0;             }         }     }     printf("Unique Number");     return 0; }

---
+++ Unique_number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[1000],k=0,i,j;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        a[k++]=n%10;
+        n/=10;
+    }
+    for(i=0;i<k;i++)
+    {
+        for(j=i+1;j<k;j++)
+        {
+            if(a[i]==a[j])
+            {
+                printf("Not Unique Number");
+                return 0;
+            }
+        }
+    }
+    printf("Unique Number");
+    return 0;
+}
